d17a58030015f00bc24cf4be8d8f61b2ebfad436
Nikolay V <nv-mail@mail.ru>
2022-04-17 20:20:47 +0300

    1. created repository by SourceTree (+Create feature) {In GH seems to be a problem with russian name of local gitfolder) 2. put file in local folder by SublimeText 3. plan to index and commit it

---
+++ Module1Part2-2v2.py
@@ -0,0 +1,11 @@
+print('Chess')
+
+x1 = int(input(' input x1 = '))
+y1 = int(input(' input y1 = '))
+x2 = int(input(' input x2 = '))
+y2 = int(input(' input y2 = '))
+
+if x1 == x2 or y1 == y2: z = 'YES'
+else: z = 'NO'
+
+print(z)
